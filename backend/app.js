e7f450d5a19b9fc08b3bf9c8f3e330deda0ecbb0
Tapu chandra bormon <topuhalder627@gmail.com>
2025-07-26 00:51:21 +0600

    read PORT from .env by adding dotenv configuration

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const captionRoutes = require('./routes/captionRoutes'); // ✅ correct path
+
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/caption', captionRoutes); // ✅ use it as middleware
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT || 5000}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
